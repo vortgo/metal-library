03a9b35e0a32eda5517c30ac5de6510346da6be0
vortgo <vortgo@gmail.com>
2020-02-15 16:11:34 +0200

    try env

--- src/router/Router.js
+++ src/router/Router.js
@@ -10,22 +10,10 @@ import SearchScreen from "../screen/SearchScreen";
 import ReviewsScreen from "../screen/ReviewsScreen";
 import AlbumScreen from "../screen/AlbumScreen";
 import BandScreen from "../screen/BandScreen";
-import {
-    Button,
-    Text,
-    Icon,
-    Item,
-    Footer,
-    FooterTab,
-    Label,
-    StyleProvider,
-} from "native-base";
-import StyleContainer from "../components/StyleContainer"
-import {StackActions, NavigationActions} from 'react-navigation';
 import SongScreen from "../screen/SongScreen";
-import DrawerComponent from "../components/Drawer/Drawer";
-import {ScrollView, View} from "react-native";
-import styles from "../components/Drawer/Drawer.style";
+import DrawerComponent from "./navigations/Drawer/Drawer";
+import Tabs from "./navigations/Tabs/Tab";
+
 
 const HomeStack = createStackNavigator({
         Home: HomeScreen,
@@ -68,76 +56,14 @@ const Drawer = createDrawerNavigator({
     Reviews: ReviewsStack,
 }, {
     drawerPosition: 'right',
-    contentComponent: props => {
-        return (
-            <View style={styles.container}>
-                <ScrollView>
-                    <View>
-                        <Text style={styles.sectionHeadingStyle}>
-                            Menu
-                        </Text>
-                        <View style={styles.navSectionStyle}>
-                            <Text style={styles.navItemStyle} onPress={() => {
-                                props.navigation.dispatch(StackActions.popToTop());
-                                props.navigation.navigate("Home")
-                            }}>
-                                Home
-                            </Text>
-                            <Text style={styles.navItemStyle} onPress={() => {
-                                props.navigation.dispatch(StackActions.popToTop());
-                                props.navigation.navigate("Search")
-                            }}>
-                                Search
-                            </Text>
-                        </View>
-                    </View>
-                </ScrollView>
-                <View style={styles.footerContainer}>
-                    <Text>This is my fixed footer</Text>
-                </View>
-            </View>
-        )
-    }
+    drawerWidth: 230,
+    contentComponent: DrawerComponent,
 });
 const Router = createBottomTabNavigator({
     Drawer: {screen: Drawer},
 }, {
     backBehavior: 'none',
-    tabBarComponent: props => {
-        return (
-            <StyleContainer>
-                <Footer style={{
-                    borderTopColor: '#414047',
-                    borderTopWidth: 1
-                }}>
-                    <FooterTab>
-                        <Button
-                            vertical
-                            active={props.navigation.state.index === 0}
-                            onPress={() => {
-                                props.navigation.dispatch(StackActions.popToTop());
-                                props.navigation.navigate("Home")
-                            }}
-                        >
-                            <Icon name="home"/>
-                            <Text>Home</Text>
-                        </Button>
-                        <Button
-                            vertical
-                            active={props.navigation.state.index === 1}
-                            onPress={() => {
-                                props.navigation.dispatch(StackActions.popToTop());
-                                props.navigation.navigate("Search");
-                            }}
-                        >
-                            <Icon name="search"/>
-                            <Text>Search</Text>
-                        </Button>
-                    </FooterTab>
-                </Footer>
-            </StyleContainer>
-        );
-    }
+    tabBarComponent: Tabs
 });
 
 
